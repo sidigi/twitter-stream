d8b05a7a57fe5f5fdfbf3ef530a64ead6ac1c8df
SidiGi <sidpunk@inbox.ru>
2018-03-03 21:36:40 +0500

    update

--- resources/views/tweets/tweet.blade.php
+++ resources/views/tweets/tweet.blade.php
@@ -8,5 +8,12 @@
 		<p><a target="_blank" href="https://twitter.com/{{ $tweet->user_screen_name }}/status/{{ $tweet->id }}">
 				View on Twitter
 			</a></p>
+		@if($tweet->media)
+			<div class="post-media">
+				@foreach($tweet->media as $media)
+					<img src="{{ $media['media_url'] }}" alt="" class="img-responsive">
+				@endforeach
+			</div>
+		@endif
 	</div>
 </div>
