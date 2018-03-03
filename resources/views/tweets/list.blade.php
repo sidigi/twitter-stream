ab1eb71a748f02f205fe3de470006c63918855de
SidiGi <sidpunk@inbox.ru>
2018-03-03 20:51:11 +0500

    update

--- resources/views/tweets/list.blade.php
+++ resources/views/tweets/list.blade.php
@@ -29,26 +29,23 @@
 				</div>
 			@endif
 			@if($subTweet)
-				@foreach($tweets as $tweet)
-					<div class="post">
-						<div class="post-header">
-							<div class="user">
-								<div class="img">
-									<img src="{{ $tweet->user_avatar_url }}"
-									     alt="">
-									<div class="name">{{ $tweet->user_screen_name }}</div>
-									<div class="created">{{ $tweet->created_at->diffForHumans() }}</div>
-
-								</div>
+				<div class="post">
+					<div class="post-header">
+						<div class="user">
+							<div class="img">
+								<img src="{{ $subTweet->user_avatar_url }}"
+								     alt="">
+								<div class="name">{{ $subTweet->user_screen_name }}</div>
+								<div class="created">{{ $subTweet->created_at->diffForHumans() }}</div>
 							</div>
 						</div>
-						<div class="post-body">
-							<p>
-								{{ $tweet->tweet_text }}
-							</p>
-						</div>
 					</div>
-				@endforeach
+					<div class="post-body">
+						<p>
+							{{ $subTweet->tweet_text }}
+						</p>
+					</div>
+				</div>
 			@endif
 		</div>
 	</div>
