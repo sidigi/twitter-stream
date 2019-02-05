6907dadb56d27be5c797462c22b5c0b6e09ea7f7
Sidigi <sidigicoder@gmail.com>
2019-02-05 13:59:16 +0500

    wip

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Spatie\LaravelTwitterStreamingApi\TwitterStreamingApiServiceProvider::class,
 
         /*
