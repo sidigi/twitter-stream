d1ccd7b7b9ff44bf528917b4a6e4622bb2d950fd
Evgeny Mikulich <sidigicoder@gmail.com>
2019-05-14 15:23:25 +0500

    laravel update (#4)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,5 @@
 const mix = require('laravel-mix');
 
-/*
-*
-*         .sass(['/admin/app.scss'], 'public/css/admin.css')
-        .sass('app.scss')
-       .webpack('/admin/app.js', 'public/js/admin.js')
-       .webpack('app.js')
-       .version(['css/admin.css', 'css/app.css' , 'js/admin.js', 'js/app.js']);
-* */
-
 /*admin*/
 mix.js('resources/assets/admin/js/app.js', 'public/build/admin/js')
    .sass('resources/assets/admin/sass/app.scss', 'public/build/admin/css')
